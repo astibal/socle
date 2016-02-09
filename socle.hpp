720d4272630c22808dd67e35dfc9d4c99a3ac625
Ales Stibal <astibal@gmail.com>
2016-02-09 09:37:06 +0100

    bump version and remove devel tag

--- socle.hpp
+++ socle.hpp
@@ -16,8 +16,8 @@
     License along with this library.
 */
 
-#define SOCLE_VERSION "0.3.11"
-#define SOCLE_DEVEL    1
+#define SOCLE_VERSION "0.3.12"
+#define SOCLE_DEVEL    0
 
 #include <common/socle_common.hpp>
 #include <common/base64.hpp>
