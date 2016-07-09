ad346bc49ccbd072ca76d190a6dfb325507d7ea5
Ales Stibal <astib@mag0.net>
2016-07-09 14:12:19 +0200

    allow set error flag with setter function

--- basecom.hpp
+++ basecom.hpp
@@ -81,11 +81,12 @@ public:
     struct epoller poller;
 
     // mark connection as invalid, owning cx is responsible to react on it
-    enum { ERROR_NONE=0, ERROR_UNSPEC=1 } err_flags;
+    typedef enum { ERROR_NONE=0, ERROR_UNSPEC=1 } err_flags;
     int  error_flag_ = ERROR_NONE;
 
     bool error() { return error_flag_ != ERROR_NONE; }
     int error_flags() { return error_flag_; };    
+    inline void error(baseCom::err_flags e) { error_flag_ = e;}
     
     virtual ~baseCom() {};
 protected:
