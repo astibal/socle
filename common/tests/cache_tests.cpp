3d5332823ad10faf49ae46971f8402c0eb5adf53
astib <astib@mag0.net>
2021-02-03 14:13:57 +0100

    ptr_cache - add erase test
    Signed-off-by: astib <astib@mag0.net>

--- common/tests/cache_tests.cpp
+++ common/tests/cache_tests.cpp
@@ -51,3 +51,29 @@ TEST(PtrCacheTest, CanDestroy) {
         std::cerr << i << " -> { " << v[i]->a << ", " << v[i]->b << "}, use count: " << v[i].use_count() << std::endl;
     }
 }
+
+TEST(PtrCacheTest, CanErase) {
+
+    constexpr int cache_size = 10;
+    constexpr int data_len = 21;
+    ptr_cache<std::string, SomeObject> c("mycache", cache_size, true);
+
+    auto v = std::vector<std::shared_ptr<SomeObject>>();
+
+    for (int i = 0; i < data_len; i++) {
+        auto a = std::make_shared<SomeObject>(i, i);
+        v.push_back(a);
+        c.set(string_format("%d", i), a);
+    }
+
+    c.erase("20");
+    ASSERT_TRUE(c.cache().size() == cache_size - 1);
+    ASSERT_TRUE(v[20].use_count() == 1);
+
+    std::cerr << "CanErase:" << std::endl;
+    for(int i = 0; i < data_len; i++) {
+        std::cerr << i << " -> { " << v[i]->a << ", " << v[i]->b << "}, use count: " << v[i].use_count() << std::endl;
+    }
+
+    ASSERT_TRUE(! c.get("20") );
+}
