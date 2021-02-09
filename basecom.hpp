29ab5c7826573c02342cab3a8e024dbe811d0cd0
astib <astib@mag0.net>
2021-02-09 20:02:15 +0100

    fix also signedness of poll timers
    Signed-off-by: astib <astib@mag0.net>

--- basecom.hpp
+++ basecom.hpp
@@ -50,8 +50,8 @@ public:
     static inline const char str_getsockname[] = "getsockname-tproxy";
     static inline const char str_getpeername[] = "getpeername-tproxy";
 
-    static inline unsigned long poll_msec = 100;
-    static inline unsigned long rescan_poll_multiplier = 2;
+    static inline long poll_msec = 100;
+    static inline long rescan_poll_multiplier = 2;
 
     int     poll_result = 0;
     baseHostCX* owner_cx_ = nullptr;
