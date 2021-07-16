dca895115563f5e18295aed117a7dffe72e2d60a
astib <astib@mag0.net>
2021-07-16 07:29:02 +0200

    socle::raw RAII guards
    Signed-off-by: astib <astib@mag0.net>

--- vars.hpp
+++ vars.hpp
@@ -1,9 +1,27 @@
-//
-// Created by astib on 15.07.21.
-//
+/*
+    Socle - Socket Library Ecosystem
+    Copyright (c) 2014, Ales Stibal <astib@mag0.net>, All rights reserved.
 
-#ifndef SMITHPROXY_VARS_HPP
-#define SMITHPROXY_VARS_HPP
+    This library  is free  software;  you can redistribute  it and/or
+    modify  it  under   the  terms of the  GNU Lesser  General Public
+    License  as published by  the   Free Software Foundation;  either
+    version 3.0 of the License, or (at your option) any later version.
+    This library is  distributed  in the hope that  it will be useful,
+    but WITHOUT ANY WARRANTY;  without  even  the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    See the GNU Lesser General Public License for more details.
+
+    You  should have received a copy of the GNU Lesser General Public
+    License along with this library.
+*/
+
+#ifndef VARS_HPP
+#define VARS_HPP
+
+
+#include <cstdio>
+#include <unistd.h>
 
 namespace socle {
 
@@ -22,6 +40,71 @@ namespace socle {
 
         return 'L';
     }
+
+    namespace raw {
+        template <class T>
+        struct lax {
+            lax() = delete;
+            lax(T&& v, std::function<void(T&)> dter): value(v), deleter(dter) {}
+            lax(T v, std::function<void(T&)> dter): value(v), deleter(dter) {}
+
+            ~lax() {
+                deleter(value);
+            }
+
+            lax(lax& r) {
+                if(&r != this) {
+                    deleter(value);
+                    value = r.value;
+                }
+            }
+            void operator=(lax const& v) {
+                deleter(value);
+                value = v;
+            }
+
+            T value;
+            std::function<void(T&)> deleter;
+        };
+
+
+        template <typename T>
+        struct var {
+            var(T&& v, std::function<void(T&)> dter): value(v), deleter(dter) {}
+            ~var() {
+                deleter(value);
+            }
+
+            T value;
+            std::function<void(T&)> deleter;
+        };
+
+        template <typename T>
+        struct unique {
+            unique(T&& v, std::function<void(T&)> dter): value(v), deleter(dter) {}
+
+            unique& operator=(unique const&) = delete;
+            unique(unique &) = delete;
+
+            ~unique() {
+                deleter(value);
+            }
+
+            T value;
+            std::function<void(T&)> deleter;
+        };
+
+
+        namespace deleter {
+
+            template <typename PT>
+            inline void free(PT& ptr) {  ::free(ptr); }
+            inline void fclose(FILE*& f) { ::fclose(f); }
+            inline void close(int& f) { ::close(f); }
+
+        }
+
+    }
 }
 
-#endif //SMITHPROXY_VARS_HPP
+#endif //VARS_HPP
