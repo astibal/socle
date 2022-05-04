feccaf8a8f670f44fad25e871846717823b3d7e2
astib <astib@mag0.net>
2022-05-04 11:02:41 +0200

    version bump
    Signed-off-by: astib <astib@mag0.net>

--- socle.hpp
+++ socle.hpp
@@ -16,8 +16,8 @@
     License along with this library.
 */
 
-#define SOCLE_VERSION "0.9.29"
-#define SOCLE_DEVEL    1
+#define SOCLE_VERSION "0.9.30"
+#define SOCLE_DEVEL    0
 
 #include <common/socle_common.hpp>
 #include <common/base64.hpp>
