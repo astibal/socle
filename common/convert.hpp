57d4a18f3699169e320dcb2e1856ba642d9987ca
astib <astib@mag0.net>
2022-10-25 23:43:01 +0200

    improve convert.hpp
    Signed-off-by: astib <astib@mag0.net>

--- common/convert.hpp
+++ common/convert.hpp
@@ -3,15 +3,41 @@
 
 #include <optional>
 #include <limits>
+#include <stdexcept>
+#include <typeinfo>
+#include <type_traits>
 
 namespace raw {
 
+    template<typename T>
+    std::size_t max_of() noexcept {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+
+        return std::numeric_limits<T>::max();
+    }
+    template<typename T>
+    std::size_t max_of(T const& t) noexcept {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+
+        return std::numeric_limits<T>::max();
+    }
+
+    class cast_overflow : public std::runtime_error {
+    public:
+        using std::runtime_error::runtime_error;
+        explicit cast_overflow() noexcept : std::runtime_error("cast overflow") {};
+        explicit cast_overflow(const char* what) noexcept : std::runtime_error(what) {};
+    };
+
     template<typename T, typename U>
-    inline std::optional<T> down_cast(std::optional<U> u) {
+    inline std::optional<T> down_cast(std::optional<U> u) noexcept {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "integral type required.");
+
         if(not u.has_value()) {
             return std::nullopt;
         }
-        else if (u > std::numeric_limits<T>::max() or u < std::numeric_limits<T>::min()) {
+        else if (u.value() > std::numeric_limits<T>::max() or u.value() < std::numeric_limits<T>::min()) {
             return std::nullopt;
         }
         else {
@@ -20,14 +46,29 @@ namespace raw {
     }
 
     template<typename T, typename U>
-    inline std::optional<T> down_cast(U u) {
+    inline std::optional<T> down_cast(U u) noexcept {
         return down_cast<T>(std::make_optional<U>(u));
     }
 
 
+    template<typename T, typename U>
+    inline T try_down_cast(U u) {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "integral type required.");
+
+        if (u > std::numeric_limits<T>::max() or u < std::numeric_limits<T>::min()) {
+            throw cast_overflow("bad_cast: out of target type bounds");
+        }
+        else {
+            return static_cast<T>(u);
+        }
+    }
 
     template<typename T, typename U>
     inline std::optional<T> to_signed_cast(std::optional<U> u) {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "integral type required.");
+
         if(not u.has_value()) {
             return std::nullopt;
         }
@@ -40,7 +81,6 @@ namespace raw {
         else {
             return std::nullopt;
         }
-
     }
 
     template<typename T, typename U>
@@ -48,25 +88,60 @@ namespace raw {
         return to_signed_cast<T>(std::make_optional<U>(u));
     }
 
+    template<typename T, typename U>
+    inline T try_to_signed_cast(U u) {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "integral type required.");
 
+        if(u <= std::numeric_limits<T>::max()) {
+            return static_cast<T>(u.value());
+        }
+        else if(u >= std::numeric_limits<T>::min()) {
+            return static_cast<T>(u - std::numeric_limits<T>::min()) + std::numeric_limits<T>::min();
+        }
+        else {
+            throw cast_overflow("bad_cast: out of target type bounds");
+        }
+    }
 
     template<typename T, typename U>
     inline std::optional<T> from_signed_cast(std::optional<U> u) {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "signed integral type required.");
+        static_assert(std::is_signed_v<U>, "sign-cast not needed, converting only signed integrals");
+        static_assert(sizeof(T) >= sizeof(U), "converting signed to unsigned for only for same, or larger size types");
+
         if(not u.has_value()) {
             return std::nullopt;
         }
-        else if(u <= std::numeric_limits<T>::max() and u >= static_cast<U>(0)) {
+        // if u is non-negative, we can convert or static-upcast
+        else if(u >= static_cast<U>(0)) {
             return static_cast<T>(u.value());
         }
         else {
             return std::nullopt;
         }
-
     }
 
     template<typename T, typename U>
     inline std::optional<T> from_signed_cast(U u) {
         return from_signed_cast<T>(std::make_optional<U>(u));
+    }
+
+
+    template<typename T, typename U>
+    inline std::optional<T> try_from_signed_cast(U u) {
+        static_assert(std::is_integral_v<T>, "integral type required.");
+        static_assert(std::is_integral_v<U>, "integral type required.");
+        static_assert(std::is_signed_v<U>, "sign-cast not needed, converting only signed integrals");
+        static_assert(sizeof(T) == sizeof(U), "converting signed to unsigned for only for same-size types");
+
+        if(u >= static_cast<U>(0)) {
+            return static_cast<T>(u);
+        }
+        else {
+            throw cast_overflow("bad_cast: casting negative to unsigned value");
+        }
     }
 
 }
--- common/tests/cast_tests.cpp
+++ common/tests/cast_tests.cpp
@@ -5,15 +5,30 @@
 
 using namespace raw;
 
+#define print_size(x) \
+    std::cout << #x"::max = " << (long long) std::numeric_limits<x>::max() << "\n"; \
+    std::cout << #x"::min = " << (long long) std::numeric_limits<x>::min() << "\n"; \
+
 TEST(DownCast, SimpleOk) {
+
+    print_size(uint16_t);
+    print_size(uint8_t);
+    print_size(int8_t);
 
     uint16_t a = 0xff;
     uint8_t b = down_cast<uint8_t>(a).value_or(0);
 
-    std::cout << string_format("%d", b);
-
+    std::cout << string_format("%d\n", (int)b);
     // can fit
     ASSERT_TRUE(b == 0xff);
+
+    b = try_down_cast<uint8_t>(a);
+    std::cout << string_format("%d\n", (int)b);
+    ASSERT_TRUE(b == 0xff);
+
+    int8_t c = down_cast<int8_t>(a).value_or(0);
+    std::cout << string_format("%d\n", (int)c);
+    ASSERT_TRUE(c == 0);
 }
 
 
@@ -26,29 +41,39 @@ TEST(DownCast, SimpleFail) {
 
     // cannot fit
     ASSERT_TRUE(b == 0);
+    ASSERT_THROW(try_down_cast<uint8_t>(a), cast_overflow);
 }
 
+
+TEST(DownCast, SimpleFail_Signed) {
+    uint16_t a = 127;
+    int8_t d = down_cast<int8_t>(a).value_or(0);
+    std::cout << string_format("%d\n", (int)d);
+    ASSERT_TRUE(d == 127);
+
+    ASSERT_NO_THROW(down_cast<int8_t>(a));
+
+    a = 128;
+    ASSERT_THROW(try_down_cast<int8_t>(a), cast_overflow);
+}
 
 TEST(FromUnsigned, Fail) {
 
     unsigned long a = std::numeric_limits<unsigned long>::max() - 1000;
     long b = to_signed_cast<long>(a).value_or(-1);
 
-
-    std::cout << string_format("%d", b);
+    std::cout << string_format("%l\n", b);
 
     // fails, because such a big unsigned can't fit signed, even the same size
     ASSERT_TRUE(b < 0);
 }
-
 
 TEST(FromUnsigned, Ok) {
 
     unsigned long a = 100000;
     long b = to_signed_cast<long>(a).value_or(0);
 
-
-    std::cout << string_format("%d", b);
+    std::cout << string_format("%l\n", b);
 
     // few thousands can easily fit signed long
     ASSERT_TRUE(b == 100000);
@@ -61,10 +86,11 @@ TEST(FromSigned, Fail) {
     unsigned long b = from_signed_cast<unsigned long>(a).value_or(0);
 
 
-    std::cout << string_format("%d", b);
+    std::cout << string_format("%ul", b);
 
     // negative cannot fit unsigned
     ASSERT_TRUE(b == 0);
+    ASSERT_THROW(try_from_signed_cast<unsigned long>(a), cast_overflow);
 }
 
 TEST(FromSigned, Ok) {
@@ -73,8 +99,46 @@ TEST(FromSigned, Ok) {
     unsigned long b = from_signed_cast<unsigned long>(a).value_or(0);
 
 
-    std::cout << string_format("%d", b);
+    std::cout << string_format("%ul", b);
 
     // negative cannot fit unsigned
     ASSERT_TRUE(b == 100000);
+}
+
+TEST(Overflow, test1) {
+    uint64_t big = 0xffffffff;
+    uint16_t x = down_cast<uint16_t>(big).value_or(max_of(x));
+    uint16_t xx = down_cast<uint16_t>(big).value_or(max_of<uint16_t>());
+
+    ASSERT_TRUE(x == std::numeric_limits<uint16_t>::max());
+    ASSERT_TRUE(xx == std::numeric_limits<uint16_t>::max());
+
+}
+
+TEST(Combine, test1) {
+    long port = -345;
+
+    // this statically assert
+    // unsigned short target = from_signed_cast<unsigned short>(port);
+
+    // down-cast will succeed, we will fit in the range of signed short
+    ASSERT_TRUE(down_cast<short>(port).value_or(-1) == -345);
+    ASSERT_TRUE(from_signed_cast<unsigned short>(down_cast<short>(port)).value_or(0) == 0);
+
+    // max-value of unsigned short is 65535
+    unsigned short target = from_signed_cast<unsigned short>(down_cast<short>(port)).value_or(max_of(target));
+    std::cout << target << "\n";
+    ASSERT_TRUE(target == 65535);
+}
+
+TEST(Combine, test2) {
+    int64_t difference = -2149999647;
+    ASSERT_THROW(try_down_cast<int32_t>(difference), cast_overflow);
+}
+
+TEST(Combine, test3) {
+    int64_t difference = 1149999647;
+    int32_t x;
+    ASSERT_NO_THROW(x = try_down_cast<int32_t>(difference));  // hehe, this might fail to throw on platforms with huge `int` :)
+    ASSERT_THROW(try_down_cast<int16_t>(x), cast_overflow);
 }
