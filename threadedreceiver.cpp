astibal/socle

commit 73c02ecec158e09c5320cca52e2869187d4d043f

fix datagram - don't allocate negative sizes

threadedreceiver.cpp
@@ -207,6 +207,10 @@ bool ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) 
     auto red = com()->read(sock, buff, buff_sz, 0);
 
     _dia("red: %d bytes from socket %d", red, sock);
+    if(red < 0) {
+        _err("read returned %d", red);
+        return false;
+    }
 
     int enk = 0;
 
@@ -318,7 +322,7 @@ void ThreadedReceiver<Worker>::on_left_new_raw(int sock) {
                 // ::send(fd2, "post2", 5, MSG_DONTWAIT);
 
 
-                add_first_datagrams(sock, creds.value());
+                if(not add_first_datagrams(sock, creds.value())) break;
 
             } else {
                 int l = dummy_read();

commit 6bdc94503b62d3b88c3ecf8c0a689d1f1e1f06b6

fix previously broken UDP receiver in 73c02ec

threadedreceiver.cpp
@@ -152,7 +152,7 @@ std::optional<SocketInfo> ThreadedReceiver<Worker>::process_anc_data(int sock, m
 
 
 template<class Worker>
-bool ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) {
+int ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) {
 
     auto session_key = pinfo.create_session_key(true);
 
@@ -177,7 +177,7 @@ bool ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) 
 
     std::shared_ptr<Datagram> entry;
     auto it = udpc->datagrams_received.find(session_key);
-    bool new_entry = true;
+    bool new_entry = 1;
 
     if(it != udpc->datagrams_received.end()) {
 
@@ -188,7 +188,7 @@ bool ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) 
             it->second = create_new_entry(sock, pinfo);
         }
         entry = it->second;
-        new_entry = false;
+        new_entry = 0;
     } else {
 
         _dia("new datagram");
@@ -209,7 +209,7 @@ bool ThreadedReceiver<Worker>::add_first_datagrams(int sock, SocketInfo& pinfo) 
     _dia("red: %d bytes from socket %d", red, sock);
     if(red < 0) {
         _err("read returned %d", red);
-        return false;
+        return -1;
     }
 
     int enk = 0;
@@ -322,7 +322,7 @@ void ThreadedReceiver<Worker>::on_left_new_raw(int sock) {
                 // ::send(fd2, "post2", 5, MSG_DONTWAIT);
 
 
-                if(not add_first_datagrams(sock, creds.value())) break;
+                if(add_first_datagrams(sock, creds.value()) < 0) break;
 
             } else {
                 int l = dummy_read();

threadedreceiver.hpp
@@ -54,7 +54,9 @@ public:
     // return  tuple:
     // 0: true if the session is new
     // 1: session key
-    bool add_first_datagrams(int sock, SocketInfo& pinfo);
+
+
+    int add_first_datagrams(int sock, SocketInfo& pinfo);
     void on_left_new_raw(int) override;
     void on_right_new_raw(int) override;
     