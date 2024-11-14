18e47b7fefa6ea51049222b6d9eb72c163f0241a
ales stibal <astib@mag0.net>
2024-11-14 18:25:25 +0100

    misc - improve performance of string_format
    - fix totally wrong legacy code

--- common/stringformat.hpp
+++ common/stringformat.hpp
@@ -23,88 +23,38 @@
 
 #include <mempool/mempool.hpp>
 
-void* mempool_realloc(void*, size_t);
+void* mempool_alloc(size_t);
 void mempool_free(void*);
 
 #pragma GCC diagnostic ignored "-Wformat-security"
 #pragma GCC diagnostic push
 
+
 template <class ... Args>
 std::string string_format(const char* format, Args ... args)
 {
+    constexpr int default_buff_sz = 2048;
+    char stack_buffer[default_buff_sz]; // intentionally no pre-init, it may be garbage as the buffer will be overwritten
+    char* buffer = stack_buffer;
 
-    int cap = 512;
-    int mul = 1;
-    int max = 20;
-    void* buffer = nullptr;
+    //  man snprintf:
+    //  The functions snprintf() and vsnprintf() write at most size bytes (including the terminating null byte ('\0')) to str.
+    //  ... and return bytes that would have been written if buffer is large enough, or < 0 on error.
+    auto written_n = snprintf(buffer, default_buff_sz, format, args...);
 
-    // data written to buffer
-    int written_n = 0;
-    int cursize = cap*mul;
+    if(written_n < 0 or written_n >= default_buff_sz) {
 
-    do {
+        buffer = (char*)mempool_alloc(written_n+1); //space for \0
+        if(not buffer) return {};
 
-        cursize = cap*mul;
-
-        buffer = mempool_realloc(buffer, cursize);
-
-        if(not buffer) {
-            // be polite
-            return "";
-        }
-
-        memset(buffer, 0, cursize);
-
-        //  man snprintf:
-        //  The functions snprintf() and vsnprintf() write at most size bytes (including the terminating null byte ('\0')) to str.
-        written_n = snprintf((char*)buffer, cursize, format, args...);
-
-        if(written_n < 0) {
-            written_n = cursize;
-        }
-
-        mul++;
-    } while(written_n >= cursize && mul <= max);
-
+        written_n = snprintf((char*)buffer, written_n+1, format, args...);
+    }
 
     // w counts in also \0 terminator
     std::string ret((const char*)buffer, written_n);
-    mempool_free(buffer);
+    if(buffer != stack_buffer) mempool_free(buffer);
 
     return ret;
-}
-
-
-template <class ... Args>
-[[nodiscard]] const char* string_format_heap(const char* format, Args ... args)
-{
-
-    int cap = 512;
-    int mul = 1;
-    int max = 20;
-    void* b = nullptr;
-
-    // data written to buffer
-    int w = 0;
-    int cursize = cap*mul;
-
-    do {
-
-        cursize = cap*mul;
-
-        b = ::realloc(b, cursize);
-        memset(b, 0, cursize);
-
-        //  man snprintf:
-        //  The functions snprintf() and vsnprintf() write at most size bytes (including the terminating null byte ('\0')) to str.
-        w = snprintf((char*)b, cursize, format, args...);
-
-        mul++;
-    } while(w >= (int)cursize && mul <= max);
-
-
-    // w counts in also \0 terminator
-    return (const char*)b;
 }
 
 
